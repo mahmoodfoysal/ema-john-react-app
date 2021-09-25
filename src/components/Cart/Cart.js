543b31555a0423eba33867b2a752f6ec03d5743c
mahmoodfoysal <foysalcse033@gmail.com>
2021-09-25 21:39:39 +0600

    Done

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,17 +3,22 @@ import './Cart.css';
 const Cart = (props) => {
     console.log(props.cart);
     const {cart} = props;
+    let totalQuantity = 0;
     let total = 0;
     for(const product of cart) {
-        total = total + product.price;
+        if(!product.quantity) {
+            product.quantity = 1;
+        }
+        total = total + product.price * product.quantity;
+        totalQuantity = totalQuantity+product.quantity;
     }
-    const shipping = 15;
+    const shipping = total > 0 ? 15: 0;
     const tax =(total+shipping)*0.10;
     const grandTotal = total+shipping+tax;
     return (
         <div>
             <h3>Order Summary</h3>
-            <h5>Item Ordered: {props.cart.length}</h5>
+            <h5>Item Ordered: {totalQuantity}</h5>
             <br />
             <p>Total: {total.toFixed(2)}</p>
             <p>Shipping: {shipping}</p>
